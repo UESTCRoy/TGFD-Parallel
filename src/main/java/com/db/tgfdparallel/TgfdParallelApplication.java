d240ee49962dee72b0639203a76ace7123c7b0ed
UESTCRoy <roywangwyx@gmail.com>
2023-01-28 16:17:53 -0500

    first commit

---
+++ src/main/java/com/db/tgfdparallel/TgfdParallelApplication.java
@@ -0,0 +1,13 @@
+package com.db.tgfdparallel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgfdParallelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgfdParallelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/db/tgfdparallel/TgfdParallelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.tgfdparallel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgfdParallelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
